b0f631008119f56d7b8d7d0207478576067544d4
Shadow21X <mathaan96@gmail.com>
2016-12-11 22:50:58 +0100

    Add files via upload
    Schießen Funktioniert !.
    Movement von Projectil ist nun in der Klasse Projectil drinne Main ist clean af now.

--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,9 @@ public class Game extends PApplet {
 	Player Player1 = new Player(this);
 	Enemy[] Enemy1 = new Enemy[9];
 	ArrayList<Projectile> schuss = new ArrayList<Projectile>();
-	boolean nigga = false;
+	boolean nigga, sh = false;
+	boolean canShoot = true;
+	int canShootCounter;
 
 	public static void main(String[] args) {
 		PApplet.main("Game");
@@ -24,10 +26,6 @@ public class Game extends PApplet {
 
 		}
 
-		// for (int i = 0; i < 10; i++) {
-		// schuss.add(new Projectile(this, Player1));
-		// }
-
 	}
 
 	public void settings() {
@@ -40,7 +38,7 @@ public class Game extends PApplet {
 
 	public void draw() {
 		background(bg);
-		schuss.add(new Projectile(this, Player1));
+
 		fill(220, 153, 255);
 		textSize(20);
 		text("FPS: " + (int) frameRate, 0, 20);
@@ -58,27 +56,47 @@ public class Game extends PApplet {
 
 		}
 
-		// Projektil Zeichnen und bei KeyEvent schießen
+		// Methode zum Schießen , KLAPPT !
+		msis();
+
+		// Projectile erstellen & zeichnen
+		Projectile kill = new Projectile(this, Player1);
 		for (int i = 0; i < schuss.size(); i++) {
-			Projectile j = schuss.get(i);
-			j.drawPro();
-			j.shoot();
-			if (j.y <= Player1.y) {
-				nigga = true;
-			}
-			if (nigga) {
-				if (key == ' ') {
-					schuss.remove(schuss.size() - 1);
-					j.drawPro();
-					j.shoot();
-					nigga = false;
-
-				}
+			kill = schuss.get(i);
+			kill.draw();
+			schuss.get(i).shoot();
+			if (schuss.get(i).y <= 0) {
+				schuss.remove(0);
 
 			}
-
 		}
+		// Zähler
+		System.out.println(schuss.size());
 
+	}
+
+	// Methode zum Schießen , KLAPPT !
+	public void msis() {
+		if (mousePressed == true) {
+			// this regulates the shooting speed
+			if (canShoot == true) {
+				schuss.add(new Projectile(this, Player1));
+				canShoot = false;
+				canShootCounter = 0;
+			}
+		}
+		// this checks if the right amount of time has passed before canShoot
+		// can = true again
+		if (canShoot == false) {
+			canShootCounter++;
+			// if the right amount of time has passed. make canShoot true
+			if (canShootCounter == 35)/*
+										 * change this number to change the
+										 * duration
+										 */ {
+				canShoot = true;
+			}
+		}
 	}
 
 }
--- src/Projectile.java
+++ src/Projectile.java
@@ -1,46 +1,39 @@
 import processing.core.PApplet;
 
 public class Projectile extends MainObject {
-	boolean sh = false;
+	// boolean sh = false;
 
 	public Projectile(PApplet d, Player player) {
 		super(d);
 		this.x = player.x + 25;
 		this.y = player.y;
-
 	}
 
-	public void drawPro() {
-		drawing.fill(255, 255, 0);
+	public void draw() {
+		drawing.noStroke();
+		drawing.fill(244, 143, 177);
 		drawing.ellipse(this.x, this.y, 10, 10);
+
 	}
 
 	public void shoot() {
+		this.y -= 2;
 
-		if (drawing.key == ' ') {
-			sh = true;
+	}
 
-		}
-		if (sh) {
-			velY = 2;
-			this.y -= velY;
-
-		}
+	public void moveLeft() {
 		if (drawing.keyPressed) {
-
 			if (drawing.key == 'a' || drawing.key == 'A') {
-				if (sh == false) {
-					this.x += -2.5f;
-				}
+				this.x += -2.5f;
 			}
-
-			if (drawing.key == 'd' || drawing.key == 'D') {
-				if (sh == false) {
-					this.x += 2.5f;
-				}
-			}
-
 		}
 	}
 
+	public void moveRight() {
+		if (drawing.keyPressed) {
+			if (drawing.key == 'd' || drawing.key == 'D') {
+				this.x += 2.5f;
+			}
+		}
+	}
 }
